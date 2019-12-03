0084f728ed3d8af75f4e8c1e84e6e518d950cb7f
Mohit Shah <mohit@glib.ai>
2019-12-03 18:43:59 +0530

    fixed minor bug

--- at16k/bin/speech_to_text.py
+++ at16k/bin/speech_to_text.py
@@ -9,19 +9,18 @@ PARSER.add_argument('-i', '--input', type=str,
                     required=True, help='Input file (wav)')
 PARSER.add_argument('-m', '--model', type=str,
                     required=True, choices=['en_8k', 'en_16k'])
+FLAGS = PARSER.parse_args()
 
-
-def main(args):
+def main():
     """
     Main
     """
-    stt = SpeechToText(args.model)
-    result = stt('./samples/test.wav')
+    stt = SpeechToText(FLAGS.model)
+    result = stt(FLAGS.input)
     print('-'*100)
-    print(result['text'])
+    print('Output:', result['text'])
     print('-'*100)
 
 
 if __name__ == '__main__':
-    FLAGS = PARSER.parse_args()
-    main(FLAGS)
+    main()
